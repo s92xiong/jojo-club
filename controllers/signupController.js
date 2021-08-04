fc16d24ad92d562d354f6dcdf5a029e0e3557d03
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-04 19:01:54 -0400

    Functionality to sign-up / register new users is now working

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -8,7 +8,7 @@ exports.signup_get = (req, res, next) => {
 };
 
 exports.signup_post = [
-  body("username").trim().isLength({ min: 1 }).escape().withMessage("Username must be at least 6 characters.").isAlphanumeric().withMessage("Username has non-alphanumeric characters"),
+  body("username").trim().isLength({ min: 1 }).escape().withMessage("Username must be at least 6 characters."),
 	body("password").trim().isLength({ min: 1 }).escape().withMessage("Password must be at least 6 characters."),
 
   (req, res, next) => {
@@ -18,7 +18,7 @@ exports.signup_post = [
       if (err) return next(err);
       // If the username already exists, re-render sign-up form
       if (!results) return res.render("signup", { title: "Sign Up" });
-      // If username does not exist, continute to register to new user to db
+      // If username does not exist, continute to register new user to db
       bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
         if (err) return next(err);
         const user = new User({
--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, minLength: 6 },
+  username: { type: String, required: true },
   password: { type: String, required: true, minLength: 6 },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
