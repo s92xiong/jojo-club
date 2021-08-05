610a1c603b5ec7ce6be9831b09e7f5cbed276fbb
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-05 13:17:14 -0400

    implemented password confirmation for signup_post, sends an error message to user is passwords do not match

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -10,8 +10,20 @@ exports.signup_get = (req, res, next) => {
 exports.signup_post = [
   body("username").trim().isLength({ min: 1 }).escape().withMessage("Username must be at least 6 characters."),
 	body("password").trim().isLength({ min: 1 }).escape().withMessage("Password must be at least 6 characters."),
+  body("confirmPassword").trim().isLength({ min: 1 }).escape().withMessage("Password must be at least 6 characters.")
+    .custom(async (value, { req }) => {
+      if (value !== req.body.password) throw new Error('Passwords must be the same');
+      return true;
+    }),
 
   (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("ERROR!");
+      return res.render("signup", { title: "Sign Up", passwordConfirmationError: "Passwords must be the same" });
+    }
+
     async.parallel({
       // Check to see if username is already taken
       someUser: function(cb) { User.find({ "username": req.body.username }).exec(cb) },
