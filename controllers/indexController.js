1b81c8f1d8a20c786608517f1e17fcd7ab8e7d55
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-08 13:00:54 -0400

    Order messages in descending order by date/time

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const Message = require("../models/messageModel");
 exports.index = async (req, res, next) => {
   try {
     // Populate message with "user" information (reference to user in model)
-    const messages = await Message.find().populate("user");
+    const messages = await Message.find().sort([["timestamp", "descending"]]).populate("user");
     return res.render('index', { title: 'Members Only', user: req.user, messages: messages });
   } catch (err) {
     return next(err);
