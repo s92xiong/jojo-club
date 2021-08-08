d68bfa78103277611352e08350aa494ad81ae560
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-07 23:17:07 -0400

    added luxon via npm to format date as a virtual for the MessageSchema

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true, minLength: 1, maxLength: 50 },
   text: { type: String, required: true, maxLength: 1000 },
   timestamp: { type: Date, default: Date.now, required: true }
+});
+
+MessageSchema.virtual("date").get(function() {
+  return DateTime.fromJSDate(this.timestamp).toFormat("yyyy-MM-dd, HH:mm");
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
