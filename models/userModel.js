a9c62ae69c2a02e6cca1ea2ee8f63df661d0d790
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-11 14:12:40 -0400

    added maxLength of 35 to username input in model and on client side

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, maxLength: 35 },
   password: { type: String, required: true },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
