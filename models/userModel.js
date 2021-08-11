904a9e5b16d903022defc71d37886370d3c6dd43
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-11 14:14:31 -0400

    creduced maxLength of username on server & client side to 20 characters

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, maxLength: 35 },
+  username: { type: String, required: true, maxLength: 20 },
   password: { type: String, required: true },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
