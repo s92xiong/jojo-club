c995ff664bd0c4ccdd6d16bd0de2e1278b2132cc
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-10 22:11:01 -0400

    updated userModel with avatar property

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
+  avatar: { type: String , required: true, enum: ["jonathan", "joseph", "jotaro", "josuke", "giorno"], default: "jonathan" }
 });
 
 module.exports = mongoose.model("User", UserSchema);
