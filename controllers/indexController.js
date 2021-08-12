917ca3f4190453f51748a869587a6229bf9b0e11
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-11 23:24:38 -0400

    changed title name of index from Members Only to The JoJo Club

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,7 @@ exports.index = async (req, res, next) => {
   try {
     // Populate message with "user" information (reference to user in model)
     const messages = await Message.find().sort([["timestamp", "descending"]]).populate("user");
-    return res.render('index', { title: 'Members Only', user: req.user, messages: messages });
+    return res.render('index', { title: 'The JoJo Club', user: req.user, messages: messages });
   } catch (err) {
     return next(err);
   }
